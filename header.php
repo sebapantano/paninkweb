2689a5d66f688066d46a19113fa8b4bddc8f0aaf
sebapantano <sebapantano@gmail.com>
2013-03-13 01:08:02 -0300

    Create header.php

---
+++ header.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />  	
+<meta content="Posicionamiento en Google yahoo bing msn aol terra , navegabilidad, Dise&ntilde;o de paginas web, web design, posicionamiento seo, sem, email marketing, marketing online, desarrollo web, crear sitio web, por que tener un sitio?,outlook, multimedia, hosting, alojamiento, flash, indexada, posicionada, garantia, tiempo de entrega, negocio, preguntas frecuentes, estudio, soluciones, buscadores, internet, programacion, publicidad online, usabilidad, accesible, adwords, newsletter, mercado libre, planes web, diseÃ±o web, empresa de diseÃ±o web, desarrollo de sitios web, sitios web corporativos, E-marketing, Posicionamiento y Marketing en Buscadores, Comercio ElectrÃ³nico, diseÃ±o web profesional, diseÃ±o web de calidad" name="keywords" />
+<meta content="Dise&ntilde;o de paginas web, web design, posicionamiento seo, sem, email marketing, marketing online, desarrollo web, crear sitio web, por que tener un sitio?,outlook, multimedia, hosting, alojamiento, flash, indexada, posicionada, garantia, tiempo de entrega, negocio, preguntas frecuentes, estudio, soluciones, buscadores, internet, programacion, publicidad online, usabilidad, accesible, adwords, newsletter, mercado libre, planes web, diseÃ±o web, empresa de diseÃ±o web, desarrollo de sitios web, sitios web corporativos, E-marketing, Posicionamiento y Marketing en Buscadores, Comercio ElectrÃ³nico, diseÃ±o web profesional, diseÃ±o web de calidad" http-equiv="DC.Keywords" />
+<meta content="Dise&ntilde;o de paginas web, web design, posicionamiento en Google yahoo msn bing aol terra, email marketing, marketing online, desarrollo web, crear sitio web, por que tener un sitio?,outlook, multimedia, hosting, alojamiento, flash, indexada, posicionada, garantia, tiempo de entrega, negocio, preguntas frecuentes, estudio, soluciones, buscadores, internet, programacion, publicidad online, usabilidad, accesible, adwords, newsletter, mercado libre, planes web, diseÃ±o web, empresa de diseÃ±o web, desarrollo de sitios web, sitios web corporativos, E-marketing, Posicionamiento y Marketing en Buscadores, Comercio ElectrÃ³nico, diseÃ±o web profesional, diseÃ±o web de calidad" name="description" />
+<meta name="title" content="dise&ntilde;o web, dise&ntilde;o de paginas web, hosting" />
+<meta name="author" content="info@panink.com.ar" />
+<meta name="robots" content="all"/>
+<meta name="GOOGLEBOT" content="index, follow, all"/>
+<meta name="copyright" content="Panink"/>
+<meta name="Distribution" content="Global"/>
+<meta name="audience" content="All"/>
+<meta name="viewport" content="width=device-width" />
+<meta name="msvalidate.01" content="6A5C516E80A6464AB2E147733D659510" />
+<title>Panink | Dise&ntilde;o Web | Posicionamiento en Google | Email Marketing | Actualizaci&oacute;n Web | </title>
+<link href="/img/favicon.ico" type="image/x-icon" rel="shortcut icon" />
+<!--[if lt IE 9]>
+<script src="dist/html5shiv.js"></script>
+<![endif]-->
+<!--[if lt IE 9]>
+<script type="text/javascript">
+   document.createElement("nav");
+   document.createElement("header");
+   document.createElement("footer");
+   document.createElement("section");
+   document.createElement("article");
+   document.createElement("aside");
+   document.createElement("hgroup");
+</script>
+<![endif]-->
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="/css/bootstrap.css"/>
+<link rel="stylesheet" type="text/css" href="/css/bootstrap-responsive.css"/>
+<link rel="stylesheet/less" type="text/css" href="/css/style.less"/>
+
+<script src="/js/jquery.js"></script>
+<script type="text/javascript" src="/js/less-1.3.3.min.js"></script>
+<script type="text/javascript" src="/js/bootstrap.js"></script>
+<script src="/js/modernizr.js"></script>
+
+<!--google analytics-->
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-28109412-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+<!--graphics slide-->
+
+<script type="text/javascript" src="/graficas/js/jquery.js"></script>
+<script type="text/javascript" src="/graficas/js/jquery.lightbox-0.5.js"></script>
+<link rel="stylesheet" type="text/css" href="/graficas/jquery.lightbox-0.5.css" media="screen" />
+<script type="text/javascript">
+    $(function() {
+        $('#gallery a').lightBox();
+    });
+</script>
+</head>
+<body>
