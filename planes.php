eb2b6989e97b2565aa515f6a4e16131a0d47ec76
sebapantano <sebapantano@gmail.com>
2013-03-13 01:25:58 -0300

    Create planes.php

---
+++ planes.php
@@ -0,0 +1,90 @@
+<?php require_once("header.php"); ?>
+<?php require_once("top.php"); ?>
+<section class="container">  
+	<img class="title" src="/img/planes-header.jpg" />
+    <h2> Planes</h2>
+   	<table class="table table-striped">
+		<tr>
+	    	<th class="empty" ></th>
+	        <th>B&Aacute;SICO</th>
+	        <th>INTERMEDIO</th>
+	        <th>AVANZADO</th>
+	    </tr>
+	    <tr>
+	    	<td>Registro de dominio.com.ar</td>
+	        <td>Si</td>
+	        <td>Si</td>
+	        <td>Si</td>
+		</tr>  
+	   	<tr>
+	    	<td>Alta en Buscadores</td>
+	        <td>Si</td>
+	        <td>Si</td>
+	        <td>Si</td>
+	   	</tr>
+		<tr>
+	    	<td>Cantidad de Secciones</td>
+	        <td>Hasta 4</td>
+	        <td>Hasta 7</td>
+	        <td>Hasta 12</td>
+	   	</tr> 
+	   	<tr>
+	    	<td>Formulario de Contacto</td>
+	        <td>B&aacute;sico</td>
+	        <td>Personalizado</td>
+	        <td>Personalizado</td>
+		</tr>  
+	     <tr>
+	    	<td >Mapa de Ubicaci&oacute;n</td>
+	        <td >No</td>
+	        <td>Si</td>
+	        <td>Si</td>
+		</tr> 
+	    <tr>
+	    	<td>Link a Redes Sociales</td>
+	        <td>Si</td>
+	        <td>Si</td>
+	        <td>Si</td>
+		</tr>  
+	    <tr>
+	    	<td >Slide de Imagenes en Inicio </td>
+	        <td >No</td>
+	        <td>Si</td>
+	        <td>Si</td>
+	   	</tr>  
+	    <tr>
+	    	<td>Google Analytics </td>
+		    <td>No</td>
+		    <td>Si</td>
+		    <td>Si</td>
+		</tr> 
+		<tr>
+			<td>Galer&iacute;a de Imagenes </td>
+		    <td >No</td>
+		    <td>No</td>
+		    <td>Si</td>
+		 </tr>  
+		 <tr>
+		 	<td>Alojamiento Web GRATIS por 6 Meses </td>
+		    <td>Si</td>
+		    <td>Si</td>
+		    <td>Si</td>
+		</tr>  
+		<tr>
+			<td >Cuentas de E-mail</td>
+		    <td>10</td>
+		    <td >15</td>
+		    <td >20</td>
+		</tr> 
+		<tr >
+			<td class="empty"></td>
+		    <td><a href="/contacto.php" class="btn btn-primary">$900</a></td>
+		    <td><a href="/contacto.php" class="btn btn-primary">$1.500</a></td>
+		    <td><a href="/contacto.php" class="btn btn-primary">$2.500</a></td>
+		</tr>
+	</table>  
+ 	<a href="/contacto.php"><img class="miniweb" src="/img/WebAllInOne.png" /></a>
+</section>     
+<?php require_once("footer.php");?>
+</body>
+</html>     
