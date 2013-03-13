7ef552b9ab712dac16948d05accf1fe78ce48c46
sebapantano <sebapantano@gmail.com>
2013-03-13 01:18:34 -0300

    Create nosotros.php

---
+++ nosotros.php
@@ -0,0 +1,127 @@
+<?php require_once("header.php"); ?>
+<?php require_once("top.php"); ?>
+<section class="container">  
+	<img class="title" src="/img/about.png" />
+    <h2>NOSOTROS</h2>
+    <div class="row p-left">	
+        <p class="span7">
+        	Somos un equipo joven en  creciente desarrollo. Nuestra experiencia nos permite proporcionar soluciones en todas las &aacute;reas relacionadas con internet acorde a las necesidades de negocio de nuestros clientes. 
+            Capaces de desarrollar todas las propuestas de aplicaciones de alta gama web. Ya sea grande o pequeÃ±o, nos tomamos su proyecto en serio, lo que le permite aprovechar al m&aacute;ximo la &uacute;ltima tecnolog&iacute;a.
+            Nos especializamos en programaci&oacute;n web, complementado con posicionamiento en los buscadores m&aacute;s reconocidos (SEO), hosting, E-Commerce, gesti&oacute;n de dominios, Email marketing y publicidad online. 
+            Desarrollamos cada uno de nuestros proyectos bas&aacute;ndonos en est&aacute;ndares web, pensando en la usabilidad y accesibilidad. Trabajamos con asesoramiento personalizado en cada proyecto obteniendo resultados 
+            de alta calidad en la programaci&oacute;n y dise&ntilde;o web de su empresa.
+        </p>
+        <div class="span5"><img class="about" src="/img/nosotros.jpg" /></div>   
+    </div>
+    <div class="span12 p-left p-right">
+    	<h1>Preguntas Frecuentes</h1>
+    	<div class="row">    
+	        <div class="span5 p-right">           
+	        	<h3>Â¿Por qu&eacute; es importante tener un sitio web?</h3>
+	            <img class="pull-right" src="/img/negocio.jpg" /> 
+	            <ul>
+	            	<li>Presencia en Internet </li>
+	                <li>Ofrecer info acerca de tu negocio</li>
+	                <li>Ampliar base de clientes</li>
+	                <li>Nuevas oportunidades de negocio</li>
+	                <li>M&iacute;nima inversi&oacute;n</li>
+	                <li>Diferenciarse de la competencia</li>
+	                <li>Tu emprendimiento disponible al p&uacute;blico las 24 horas y los 365 d&iacute;as del a&ntilde;o</li>
+				</ul>
+			</div>
+			<div class="span1"></div>
+			<div class="span6">
+				<h3>Â¿C&oacute;mo configuro las cuentas de e-mail en Outlook Express?</h3>
+	           	<img class="pull-left" src="/img/outlook.jpg" /> 
+	            <p>
+	              Para tu comodidad, te ofrecemos configurar tus nuevas cuentas de correo electr&oacute;nico en Outlook Express.
+	              Te detallamos paso a paso las instrucciones para tener tu correo funcionando en pocos minutos, de manera simple y r&aacute;pida.
+	              Te explicamos c&oacute;mo hacerlo <a href="/outlook/outlook.php" style="text-decoration:underline">aqu&iacute;</a>
+				</p>
+			</div>
+		</div>	
+		<div class="row">
+			<div class="span5 p-right">  
+				<h3>Mi negocio es peque&ntilde;o Â¿vale la pena que tenga mi p&aacute;gina web?</h3>
+	            <img class="pull-right p-left" src="/img/pyme.jpg" />   
+	            <p>
+	              De cada uno depende que su negocio se vuelva grande y productivo &oacute; que permanezca siendo peque&ntilde;o.
+	              Internet es para TODOS y TODOS podemos sacar provecho de ella.<br>
+	              La idea de que los sitios web son solamente para grandes empresas, ya quedo sin sentido.
+	              El tener su propio sitio Web posibilita que nuevos clientes tomen contacto con uds y conozcan y adquieran sus productos eliminando las barreras de las distancias.
+				</p>
+			</div>
+			<div class="span1"></div>
+			<div class="span6">
+				<h3>Â¿Cu&aacute;l es el tiempo de entrega?</h3>
+				<img class="pull-left" src="/img/calendar.jpg" />
+				<p>
+					El tiempo de entrega de su sitio web depende en principio de la complejidad del mismo, para cada plan de dise&ntilde;o tenemos estipulado 
+                    un tiempo de desarrollo y entrega estimado.<br>
+                    La demora puede rondar desde los 15 d&iacute;as hasta los 2 meses. Es muy relativa esta demora por lo que le recomendamos ponerse 
+                    en contacto con nosotros y que nos cuente sobre su proyecto.
+				</p>
+			</div>
+		</div>
+		<div class="row">
+			<div class="span5 p-right">
+				<h3>Â¿Por qu&eacute; no utilizar FLASH?</h3>
+				<img class="pull-left p-right" src="/img/flash.jpg" />
+				<p>Principalmente no se dise&ntilde;a tanto con este programa ya que las p&aacute;ginas realizadas en Flash no son bien indexadas y posicionadas en los buscadores. Otras de las razones por lo cual no se utiliza Flash son:</p>
+				<ul>
+                	<li> Problemas de visualizaci&oacute;n: Los tel&eacute;fonos m&oacute;viles no tienen soporte para Flash,  por lo cual se dificulta la visualizaci&oacute;n del contenido.</li>
+                    <li>No es accesible: Los textos no pueden copiarse. Tampoco son indexados por los buscadores, por lo cual la web es ignorada y no se posiciona.</li>
+                    <li>Carga lenta: Los sitios en Flash son m&aacute;s pesados y tardan en cargar. Consecuencia: no se puede ver el contenido hasta que se cargue por completo y esto puede llevar al usuario a abandonar el sitio.</li>
+                    <li>No cumple los est&aacute;ndares web: no valora las necesidades de los usuarios con discapacidades y dificulta la navegaci&oacute;n de todas las maneras posibles.</li>
+				</ul>  
+			</div>
+			<div class="span1"></div>
+			<div class="span6">
+				<h3>Â¿Ofrecen garant&iacute;a?</h3>
+				<img class="pull-left" src="/img/garantia.jpg" />
+				<p class="info">
+	                1) Sobre el sitio y su funcionamiento:
+                    Garantizamos llegar al dise&ntilde;o web de su agrado, y no daremos por finalizado el trabajo hasta lograrlo. Tambi&eacute;n nos encargaremos que el funcionamiento del mismo sea &oacute;ptimo, de carga r&aacute;pida y sin ca&iacute;das del servicio.<br>
+                    2) Devoluci&oacute;n del dinero: 
+                    En el caso que intentemos de varias formas y no logremos el dise&ntilde;o de su agrado, se analizar&aacute; la situaci&oacute;n puntualmente y se har&aacute; la entrega del dinero,
+                    hasta un 40% del monto abonado hasta el momento de la baja.<br>
+                    3) Soporte para nuestros clientes:
+                   Estaremos siempre disponibles y atentos para resolver / atender inquietudes referentes estrictamente a su p&aacute;gina web y servicios de la misma (Alta de correos, consultas sobre funcionamiento de su panel de admin...)
+				</p>
+			</div>
+		</div>  
+        <div class="row">
+        	<div class="span5 p-right">
+        		<h3>Â¿Qu&eacute; materiales necesitan de mi parte?</h3>
+        		<img class="pull-right" src="/img/multimedia.jpg" />
+        		<p class="info">
+                	1) Redacci&oacute;n de textos:
+                    Vamos a necesitar de tu parte los textos para la p&aacute;gina web. Nosotros podemos asesorarte, pero en definitiva lo mejor ser&aacute; que redactes
+                    vos mismo ya que nadie sabe mejor de tu rubro/actividad que vos.<br>
+                    2) Multimedia:
+                    Nos deber&aacute;s entregar las fotos a incluir en el sitio. Si el proyecto inclu&iacute;a adem&aacute;s videos y audios, tambi&eacute;n deber&aacute;s enviarnos esos materiales.
+				</p>
+        	</div>
+        	<div class="span1"></div>
+        	<div class="span6">
+        		<h3>Â¿C&oacute;mo elegir un buen Hosting?</h3>
+        		<img class="pull-left p-right" src="/img/host.jpg" />
+        		<p>Lo importante a tener en cuenta a la hora de elegir un alojamiento web es:</p> 
+				<ul>
+                	<li>Conectividad: Es un factor importante ya que influye directamente en que el sitio se vea r&aacute;pido y bien desde cualquier parte del mundo. </li>
+                    <li>Transferencia de datos: La transferencia mensual se consume a medida que se visita tu sitio web, por ello es fundamental tener en cuenta cual es l&iacute;mite para no generar un costo extra o suspensi&oacute;n del servicio.</li>
+                    <li>Espacio y funcionamiento de las cuentas de e-mail: Conocer el l&iacute;mite de espacio de las casillas, para evitar el rechazo de e-mails por tener una cuenta con espacio insuficiente.</li> 
+				</ul>              
+        	</div>
+        </div>                 
+	</div>
+</section>
+<div class="clear"></div>
+<div id="footer"> 
+	<?php require_once("footer.php");?>
+</div>
+</body>
+</html>
+
+
+
