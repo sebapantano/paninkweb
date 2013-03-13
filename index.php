a629e7459ba17fd64fd81021f6941535bd1a838f
sebapantano <sebapantano@gmail.com>
2013-03-13 01:18:18 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,78 @@
+<?php require_once("header.php"); ?>
+<?php require_once("top.php"); ?>
+<section class="container">  
+   	<?php require_once("slide.php");?>
+    <div class="row-fluid">
+    	<div id="webdesign" class="span12 p-left">
+ 			<a href="/portfolio.php" style="color:#666666">	
+ 				<div class="span7">
+           			<h1>Dise&ntilde;o de Paginas Web</h1>
+    	    		<p>
+        				Desarrollamos sitios web que agregan valor a su marca y lo 
+        				diferencian de su competencia. Nuestra experiencia y compromiso 
+        				le aseguran un trabajo de alta calidad. Dise&ntilde;amos y desarrollamos aplicaciones web limpias e intuitivas teniendo en cuenta la <strong>usabilidad, accesibilidad y navegabilidad</strong>. Convertimos su p&aacute;gina web en un canal de comunicaci&oacute;n directa con sus clientes.
+            		</p>
+            	</div>		
+            	<div class="span5"><img src="/img/foto1.jpg" alt="Dise&ntilde;o Web" /></div>
+			</a>
+        </div>
+        <div class="row p-left">
+            <div class="p-right span3"><img id="positioning"  src="/img/foto2.jpg" alt="Posicionamiento"  /></div>
+            <div class="span9 p-right">
+	            <h1>Posicionamientos SEO y SEM en Buscadores</h1>
+    	        <p>
+        	        Posicionamos su sitio web en los primeros buscadores, con el objetivo de obtener un mejor rendimiento del mismo y, a su vez, poder acceder a millones de posibles clientes. Asimismo, este servicio le brinda la posibilidad de tener un seguimiento y evaluaci&oacute;n estad&iacute;stica del funcionamiento de su p&aacute;gina. Analizamos el comportamiento de los usuarios y las fuentes de tr&aacute;fico de su web con herramientas como Google Analytics, entre otras.
+            	</p>
+			</div>
+        </div>
+        <div id="mkt" class="row p-left">
+			<a href="/emarketing/emarketing.php" style="color:#666666">
+           		<div class="span8">
+            		<h1>Email Marketing y Redes Sociales</h1>
+            		<p>
+                		Ofrecemos estrategias de comunicaci&oacute;n 2.0 que le permitir&aacute;n arribar a su p&uacute;blico objetivo con propuestas de valor agregado. Es necesario contar con una estrategia de marketing que abarque desde el envio de e-mails hasta las redes sociales, con el prop&oacute;sito de adquirir nuevos clientes o convencer a los clientes actuales para adquirir su servicio/producto de inmediato.  
+	            	</p>
+	            </div>	
+            	<div class="span4"><img src="/img/foto3.jpg"  alt="E-marketing"/></div>
+			</a>	
+        </div>
+        <div  class="row p-left">
+            <div class="span3"><img id="shop-img" src="/img/ml.jpg" alt="Mercado Libre" /></div>
+            <div class="span9 p-right">
+            	<h1>Su Negocio las 24hs</h1>
+            	<p>
+                	Una tienda virtual le permite acceder a un nuevo canal de venta complementario, que pondr&aacute; su negocio al alcance de millones de potenciales compradores, en todo el mundo, a toda hora. Hoy en d&iacute;a, la consulta de precios de productos y servicios se realiza, antes  que en cualquier otro lado, por medio del sitio de Mercado Libre. Es por eso que en Panink le ofrecemos esta ventaja que le permitir&aacute;, a travÃ©s de nuestros sistemas de pago, vender a cualquier punto dentro y fuera del pa&iacute;s a un muy bajo costo.
+            	</p>
+            </div>	
+        </div>
+         <div id="hosting" class="row p-left">
+         	<div class="span8">
+            	<h1 >Alojamiento y Mantenimiento Web</h1>
+            	<p>
+                	Nuestro servicio de alojamiento es un respaldo seguro y de alta calidad para su negocio. Una soluci&oacute;n profesional para sus comunicaciones via mail y web con una disponibilidad cont&iacute;nua.  
+	                El crecimiento en las ventas a trav&eacute;s de internet ha generado que cualquier actualizaci&oacute;n de contenidos comerciales se vean reflejados en la web. Esta acci&oacute;n adem&aacute;s ampl&iacute;a el tiempo de visita en su sitio y acrecienta sus posibilidades de escalar posiciones dentro de los buscadores. 
+	            </p>
+	        </div>
+            <div class="span3"><img class="p-left" src="/img/foto4.jpg"  alt="Alojamiento/Hosting"/></div>
+        </div>
+         <div class="row p-left">
+            <div class="span3"><img id="outsourcing"  src="/img/foto5.jpg" alt="Partners/Outsourcing" /></div>
+            <div class="span9 p-right">
+            	<h1 >Outsourcing</h1>
+            	<p>
+               		Colaboramos conjuntamente con Estudios de dise&ntilde;o gr&aacute;fico y publicidad, brindando soluciones externas de desarrollo web integral de manera profesional y estrat&eacute;gica, a trav&eacute;s de  nuestros conocimientos y experiencia. Si le interesa hacer una alianza, no dude en ponerse en <a href="contacto/contacto.php">contacto</a>.
+            	</p>
+			</div>
+        </div>
+         <div id="publicidad" class="row p-left">
+         	<div class="span8">
+            	<h1 >Dise&ntilde;o Digital</h1>
+            	<p>
+             		Una pieza gr&aacute;fica es una herramienta muy importante de comunicaci&oacute;n institucional. Esta puede variar en soporte y formato seg&uacute;n sus necesidades. Siempre tiene un valor funcional y simb&oacute;lico que no se debe descuidar. Es importante comprender que la publicidad busca: <strong>COMUNICAR</strong>. Nuestro objetivo es lograr que la pieza gr&aacute;fica  responda al concepto requerido por el cliente, acompa&ntilde;ada de originalidad y creatividad coherente. <strong><a href="graficas/graficas.php" style="text-decoration:underline;">Conozca nuestros trabajos</a></strong>.
+          		</p>
+          	</div>	
+            <div class="span3"><img class="p-left" src="/img/foto6.jpg"  alt="DiseÃ±o Grafico-Digital"/></div>
+        </div>
+	</div>
+</section>
+<?php require_once("footer.php");?>
