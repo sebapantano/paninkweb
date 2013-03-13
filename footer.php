98615ab78d37e03304c21796dea5b127e7f7361a
sebapantano <sebapantano@gmail.com>
2013-03-13 01:25:15 -0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,23 +1,23 @@
 <footer>
 <div class="container">
-  <div class="row-fluid">
+	<div class="row-fluid">
 		<nav class="menu-footer span9">
-	    	<ul>
+	    		<ul>
 				<li><a class="home" href="/">Inicio |</a></li>
-	        	<li><a class="portfolio" href="/portfolio.php">Portfolio |</a></li>
-		    	<li><a class="graphics" href="/graficas/graficas.php">Graficas  |</a></li>   
+		        	<li><a class="portfolio" href="/portfolio.php">Portfolio |</a></li>
+			    	<li><a class="graphics" href="/graficas/graficas.php">Graficas  |</a></li>   
 				<li><a class="emarketing" href="/emarketing/emarketing.php">E-Marketing  |</a></li>   
-		        <li><a class="plans" href="/planes.php">Planes |</a></li>              
-		        <li><a class="about" href="/nosotros.php">Nosotros |</a></li>
-		        <li><a class="contact" href="/contacto.php">Contacto</a></li>
-	    	</ul>
-	    </nav>	  
-	    <div class="follow span3">
-	    	<a href="http://www.facebook.com/profile.php?id=100003309489706" target="_blank"><img src="/img/facebook.jpg" alt="Facebook Logo"title="Facebook"/> </a>
-	    	<a href="http://twitter.com/#!/PaninkWebDesign" target="_blank"><img src="/img/twitter.jpg" title="Twitter" alt="Twitter Logo" /></a>
-	    	<a href="http://www.linkedin.com/company/panink-web-design/" target="_blank"> <img src="/img/linkedin.jpg" title="LinkedIn" alt="LinkedIn Logo" /></a>
+			        <li><a class="plans" href="/planes.php">Planes |</a></li>              
+			        <li><a class="about" href="/nosotros.php">Nosotros |</a></li>
+			        <li><a class="contact" href="/contacto.php">Contacto</a></li>
+	    		</ul>
+	    	</nav>	  
+	    	<div class="follow span3">
+	    		<a href="http://www.facebook.com/profile.php?id=100003309489706" target="_blank"><img src="/img/facebook.jpg" alt="Facebook Logo"title="Facebook"/> </a>
+	    		<a href="http://twitter.com/#!/PaninkWebDesign" target="_blank"><img src="/img/twitter.jpg" title="Twitter" alt="Twitter Logo" /></a>
+	    		<a href="http://www.linkedin.com/company/panink-web-design/" target="_blank"> <img src="/img/linkedin.jpg" title="LinkedIn" alt="LinkedIn Logo" /></a>
 		</div>
-	    <p>Copyright &copy; 2011 - 2013| Panink Web Design |<a  style="text-decoration:none; color:#999" href="mailto:info@panink.com.ar" > info@panink.com.ar</a></p>  
+		<p>Copyright &copy; 2011 - 2013| Panink Web Design |<a  style="text-decoration:none; color:#999" href="mailto:info@panink.com.ar" > info@panink.com.ar</a></p>  
 	</div>
 </div>      
 </footer>
